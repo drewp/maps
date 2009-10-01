2251bdae9f65a5b12c9459f1fd3e2ad1735fc788
drewp <drewp@bigasterisk.com>
2009-10-01 01:26:26 -0700

    send geo name via sms to the other person
    Ignore-this: 1833c7adde29e06576a27fae29f989c8
    
    darcs-hash:20091001082626-312f9-3ca3102d3cd7f948eb8e7c885d211efbe4b222d6.gz

--- map2.py
+++ map2.py
@@ -8,6 +8,11 @@ from __future__ import division
 import sys, web, time, os, urllib,jsonlib
 from xml.utils import iso8601
 from web.contrib.template import render_genshi
+import restkit
+
+# svn checkout http://geopy.googlecode.com/svn/branches/reverse-geocode geopy
+sys.path.append('/my/site/maps/geopy/build/lib.linux-x86_64-2.6')
+from geopy import geocoders
 
 render = render_genshi('templates', auto_reload=True)
 
@@ -38,13 +43,45 @@ class update(object):
         # u'altitude': 0L, u'longitude': Decimal('-120.000000'),
         # u'errorCode': 0L, u'latitude': Decimal('35.000000'),
         # u'velocity': 0L, u'vertAccuracy': 0L,
-        # u'heading': 0L}
+        # u'heading': 0L, "user": "http://bigasterisk.com/foaf.rdf#drewp"}
 
+        d = web.data()
         f = open("updates.log", "a")
-        f.write(web.data()+"\n")
+        f.write(d+"\n")
         f.close()
 
+        d = jsonlib.read(d)
+        name = placeName(d['latitude'], d['longitude'])
+
+        c3po = restkit.Resource('http://bang:9040/')
+
+        tellUsers = set([
+            'http://bigasterisk.com/foaf.rdf#drewp',
+            'http://bigasterisk.com/kelsi/foaf.rdf#kelsi',
+            ])
+        tellUsers.discard(d['user'])
+
+        for u in tellUsers:
+            c3po.post(path='', payload={
+                'user' : u,
+                'msg' : '%s position is %s' % (foafName(d['user']), name),
+                'mode' : 'sms'
+            },
+                      headers={'content-type' :
+                               'application/x-www-form-urlencoded'}
+                  )
+            # "eta home in 15 mins"
+
         return 'ok'
+
+def foafName(uri):
+    # todo
+    return {'http://bigasterisk.com/foaf.rdf#drewp' : 'Drew',
+            'http://bigasterisk.com/kelsi/foaf.rdf#kelsi' : 'Kelsi'}[uri]
+
+def placeName(lat, long):
+    return geocoders.GeoNames().reverse((lat, long))[0]
+
     
 if __name__ == '__main__':
     sys.argv.append("9033")
