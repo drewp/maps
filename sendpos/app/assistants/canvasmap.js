4a81bced072c073533cc8ef1ff3f32aca94c2bde
drewp <drewp@bigasterisk.com>
2011-03-28 00:19:20 -0700

    prettier rendering, mouse scroll/zoom support, curved trail lines
    Ignore-this: d515f9758db9eb7b9ab5c66ecfc14223
    
    darcs-hash:20110328071920-312f9-e60a57000658ae69c1310e9ed1171b72efacc81a.gz

--- sendpos/app/assistants/canvasmap.js
+++ sendpos/app/assistants/canvasmap.js
@@ -1,12 +1,14 @@
 
 function dot(ctx, pt, r, width, fill, stroke) {
     ctx.fillStyle = fill;
-    ctx.strokeStyle = stroke;
     ctx.lineWidth = width;
     ctx.beginPath();
     ctx.arc(pt.x, pt.y, r, 0, 2*Math.PI, true);
     ctx.fill();
-    ctx.stroke();
+    if (stroke) {
+	ctx.strokeStyle = stroke;
+	ctx.stroke();
+    }
 }
 
 function findExtent(placeLocs) {
@@ -30,8 +32,8 @@ function findExtent(placeLocs) {
 
 function Grid(coords) {
     this.draw = function (ctx, canvas) {
-	ctx.strokeStyle = "#888";
-	ctx.lineWidth=.5;
+	ctx.strokeStyle = "#aaa";
+	ctx.lineWidth=.2;
 	ctx.beginPath();
 	for (var wx = coords.worldExtent.minX; wx < coords.worldExtent.maxX; wx += .02 /*deg*/) {
 	    var cx = coords.world2canvas.transformPoint(Point(wx, 0)).x;
@@ -52,25 +54,46 @@ function Grid(coords) {
 }
 
 function Places(coords, places) {
-
-
     placeCenters = new RTree(); // place center points -> place obj
     $.each(places, function (i, pl) {
+	var box = {x: pl[1][1], y: pl[1][0], w: .0001, h:.0001};
 
-	var box = {x: pl[1][1], y: pl[1][0], 
-		   w: .0001, h:.0001 // these will be the text extent of the label
-		  };
-
-	placeCenters.insert(box, pl);
-	pl.worldPoint = Point(pl[1][1], pl[1][0]);
+	placeCenters.insert(box, {label: pl[0], order: i, worldPoint: Point(pl[1][1], pl[1][0])});
     });
 
     function searchVisiblePlaces(coords) {
 	var c2w = coords.getCanvas2world();
-	var wmin = c2w.transformPoint(Point(coords.canMinX, coords.canMaxY));
+	var wmin = c2w.transformPoint(
+	    Point(coords.canMinX - 200, // display text even when the point is off left
+		  coords.canMaxY));
 	var wmax = c2w.transformPoint(Point(coords.canMaxX, coords.canMinY));
 	var worldView = {x: wmin.x, y: wmin.y, w: wmax.x - wmin.x, h: wmax.y - wmin.y};
 	return placeCenters.search(worldView);
+    }
+
+    function selectPosition(placeLabels, cp, width) {
+	// todo: if the label overlaps with another label or with a
+	// trail, try other positions in a circle around the
+	// point. This will look good when the label animates into
+	// something and it spins out of the way
+	var box;
+	var found = false;
+	var foundDist = 0;
+	$.each([0, 2, -2, 4, -4], function (dist, ypush) {
+	    if (found) { 
+		return;
+	    }
+	    box = {x:cp.x+5, y: cp.y + 3 + ypush, w: width, h: 12};
+	    if (!placeLabels.search(box).length) {
+		found = true;
+		foundDist = dist;
+	    }
+	});
+	if (!found) {
+	    return null;
+	}
+	box.foundDist = foundDist;
+	return box;
     }
 
     this.draw = function (ctx, canvas) {
@@ -79,30 +102,28 @@ function Places(coords, places) {
 
 	placeLabels = new RTree(); // canvas rectangles I've drawn -> place obj
 
+	visiblePlaces.sort(function (a,b) { return a.order - b.order });
+
 	$.each(visiblePlaces, function(i, place) {	
-	    // run these in importance order, use rtree to suppress
-	    // any that would intersect with more-important ones. but
-	    // if the label overlaps with another label or with a
-	    // trail, try other positions in a circle around the
-	    // point. This will look good when the label animates into
-	    // something and it spins out of the way
-
-
 	    var cp = coords.world2canvas.transformPoint(place.worldPoint);
 
-	    var name = place[0];
+	    var name = place.label;
 	    var width = ctx.measureText(name).width;
 	    
 	    dot(ctx, cp, 3, 1, 'black', 'red');
 
-	    var box = {x:cp.x+5, y: cp.y+5, w: width, h: 12};
-	    if (placeLabels.search(box).length) {
+	    var box = selectPosition(placeLabels, cp, width);
+	    if (!box) {
 		return;
 	    }
 	    
-	    ctx.fillStyle = "black";
+	    ctx.fillStyle = "rgba(0,0,0,"+(1 - box.foundDist / 8)+")";
+
 	    ctx.font = "12px sans-serif";
-	    ctx.fillText(name, cp.x+5, cp.y+5);
+	    ctx.textAlign = "left";
+	    ctx.textBaseline = "alphabetic";
+
+	    ctx.fillText(name, box.x, box.y);
 
 	    placeLabels.insert(box, place);
 	});
@@ -110,27 +131,107 @@ function Places(coords, places) {
 }
 
 function Trails(coords, trailPoints) {
-    this.draw = function(ctx, canvas) {
+    function getControlPoints(x0,y0,x1,y1,x2,y2,t){
+	// from http://scaledinnovation.com/analytics/splines/splines.html
+	/*
+	  Copyright 2010 by Robin W. Spencer
+	  
+	  This program is free software: you can redistribute it and/or modify
+	  it under the terms of the GNU General Public License as published by
+	  the Free Software Foundation, either version 3 of the License, or
+	  (at your option) any later version.
+	  
+	  This program is distributed in the hope that it will be useful,
+	  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	  GNU General Public License for more details.
+	  
+	  You can find a copy of the GNU General Public License
+	  at http://www.gnu.org/licenses/.
+	  
+	*/
 
-	$.each(trailPoints, function (name, pts) {
+	//  x0,y0,x1,y1 are the coordinates of the end (knot) pts of this segment
+	//  x2,y2 is the next knot -- not connected here but needed to calculate p2
+	//  p1 is the control point calculated here, from x1 back toward x0.
+	//  p2 is the next control point, calculated here and returned to become the 
+	//  next segment's p1.
+	//  t is the 'tension' which controls how far the control points spread.
+	
+	//  Scaling factors: distances from this knot to the previous and following knots.
+	var d01=Math.sqrt(Math.pow(x1-x0,2)+Math.pow(y1-y0,2));
+	var d12=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+	
+	var fa=t*d01/(d01+d12);
+	var fb=t-fa;
+	
+	var p1x=x1+fa*(x0-x2);
+	var p1y=y1+fa*(y0-y2);
+	
+	var p2x=x1-fb*(x0-x2);
+	var p2y=y1-fb*(y0-y2);  
+	
+	return [p1x,p1y,p2x,p2y]
+    }
 
-	    ctx.strokeStyle = "#000000";
-	    ctx.lineWidth=3;
+    function makeCvs(canvasPoints) {
+	var cvs = []; // flattened [x,y,x,y,...]
+	for (var i=0; i<canvasPoints.length-2; i+=1) {
+	    cvs = cvs.concat(
+		getControlPoints(canvasPoints[i].x, canvasPoints[i].y,
+				 canvasPoints[i+1].x, canvasPoints[i+1].y,
+				 canvasPoints[i+2].x, canvasPoints[i+2].y,
+				 .5));
+	}
+	return cvs;
+    }
+
+    function drawCurve(ctx, canvasPoints, cvs) {
+	// adapted from
+	// http://scaledinnovation.com/analytics/splines/splines.html
+	// but my canvasPoints are objects with x,y, not unpacked
+	// coordinate pairs
+	var n = canvasPoints.length;
+	ctx.beginPath();
+        ctx.moveTo(canvasPoints[0].x,canvasPoints[0].y);
+        ctx.quadraticCurveTo(cvs[0],cvs[1], canvasPoints[1].x,canvasPoints[1].y);
+        ctx.stroke();
+        ctx.closePath();
+
+	for(var i=2; i<n*2-5; i+=2){
 	    ctx.beginPath();
+	    ctx.moveTo(canvasPoints[i/2].x, canvasPoints[i/2].y);
+	    ctx.bezierCurveTo(cvs[2*i-2], cvs[2*i-1], 
+			      cvs[2*i], cvs[2*i+1],
+			      canvasPoints[i/2+1].x,
+			      canvasPoints[i/2+1].y);
+	    ctx.stroke();
+	    ctx.closePath();
+	}
 
+	ctx.beginPath();
+        ctx.moveTo(canvasPoints[n-1].x, canvasPoints[n-1].y);
+        ctx.quadraticCurveTo(cvs[2*n*2-10], cvs[2*n*2-9],
+			     canvasPoints[n-2].x, canvasPoints[n-2].y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    this.draw = function(ctx, canvas) {
+	$.each(trailPoints, function (name, pts) {
+	    var initial = name.replace(/.*#(.).*/,"$1").toUpperCase();
 	    var dotArgs = [];
+	    var canvasPoints = []; // Point objects
 
 	    $.each(pts, function (i, pt) {
 		var cp = coords.world2canvas.transformPoint(Point(pt.longitude, pt.latitude));
-
-		if (i == 0) {
-		    ctx.moveTo(cp.x, cp.y);
-		} else {
-		    ctx.lineTo(cp.x, cp.y);
-		}
-		dotArgs.push([cp, 3, 1, 'green', 'black']);
+		canvasPoints.push(cp);
+		dotArgs.push([cp, 3, 1, 'rgba(0,100,0,.6)', null]);
 	    });
-	    ctx.stroke();
+
+	    ctx.strokeStyle = {K: "rgba(1,0,0,.2)", D: "rgba(0,1,0,.2)"}[initial];
+	    ctx.lineWidth=5;
+	    drawCurve(ctx, canvasPoints, makeCvs(canvasPoints));
 
 	    $.each(dotArgs, function (i, a) {
 		dot(ctx, a[0], a[1], a[2], a[3], a[4]);
@@ -139,17 +240,12 @@ function Trails(coords, trailPoints) {
 	    var lastPoint = pts[pts.length - 1]
 	    var cp = coords.world2canvas.transformPoint(Point(lastPoint.longitude, lastPoint.latitude));
 
-	    var initial = name.replace(/.*#(.).*/,"$1").toUpperCase();
-
 	    dot(ctx, cp, 10, 1, {K: 'pink', D: 'lightgreen'}[initial], 'black');
 
 	    ctx.fillStyle = "black";
 	    ctx.font = "16px sans-serif";
 	    ctx.fillText(initial, cp.x-6, cp.y+6);
-
 	});
-
-
     }
 }
 
@@ -169,15 +265,14 @@ function centerPoint(params, pt, redraw) {
 
     var startTime = 1 * new Date();
     var endTime = startTime + 2000;
-    var centerStart = Point(params.cx, params.cy);
+    var centerStart = params.center;
     
     function loop() {
 	var t = 1 * new Date();
 	var f = (t - startTime) / (endTime - startTime);
 
 	var cur = centerStart.add(pt.subtract(centerStart).scale(f));
-	params.cx = cur.x;
-	params.cy = cur.y;
+	params.center = cur;
 	redraw();
 	
 	if (f < 1) {
@@ -189,12 +284,54 @@ function centerPoint(params, pt, redraw) {
 
 function xformFromParams(params, diameter) {
     var scl = diameter/5*Math.pow(params.scale, 2);
-    return Matrix.scale(scl, -scl*2, Point(params.cx, params.cy))
+    return Matrix.scale(scl, -scl*2, params.center)
 	.translate(diameter*.625/scl, -diameter*.25/scl);
 }
 
+function startStomp(onMessage) {
+    var stomp = new STOMPClient();
+    stomp.onconnectedframe = function () {
+	stomp.subscribe('view');
+	$.post("updateTrails")
+    };
+    stomp.onmessageframe = function(frame) {
+	var data = JSON.parse(frame.body);
+	onMessage(data);
+    };
+    stomp.connect('localhost', 61614);
+}
+
+function getPosition(canvas, e) { // from inside gury.js, except I return Point
+    var left = 0, top = 0;
+    
+    if (canvas.offsetParent) {
+        while (canvas) {
+            left += canvas.offsetLeft;
+            top += canvas.offsetTop;
+            canvas = canvas.offsetParent;
+        }
+    }
+    
+    return Point(e.pageX - left,
+		 e.pageY - top);
+}
+
+function zoomAbout(canvas, params, coords, factor, anchorCanvas) {
+    /*
+      change scale by this factor, keeping the given canvas position anchored
+    */
+    var anchorWorld = coords.getCanvas2world().transformPoint(anchorCanvas);
+    params.scale *= factor;
+    coords.recalc(params);
+
+    var newWorld = coords.getCanvas2world().transformPoint(anchorCanvas);
+    var fix = anchorWorld.subtract(newWorld);
+    params.center = params.center.add(fix);
+    coords.recalc(params);
+}
+
 $(function() {
-    var params = {"cx":-122.346,"cy":37.547,"scale":7.849};
+    var params = {center: Point(-122.346, 37.547) ,"scale":7.849};
 
     var worldExtent = findExtent(placeLoc);
 
@@ -210,24 +347,50 @@ $(function() {
     };
     coords.recalc(params);
 
-
     var trailPoints = {};
 
     g = $g('mapArea');
+
     g.size(coords.canMaxX, coords.canMaxY)
 	.add(new Grid(coords))
-	.add(new Places(coords, placeLoc)) // should be getting these from server
+	.add('places', new Places(coords, placeLoc)) // should be getting these from server
 	.add(new Trails(coords, trailPoints))
 	.draw();
 
-    $("#scale").slider({min: 1, max: 20, step: .001});
-    $("#cx").slider({min: -125, max: -120, step: .001});
-    $("#cy").slider({min:37, max: 38, step: .001});
+    var dragStartWorld;
+    function onMove(e) {
+	var pos = getPosition(g.canvas, e);
+	currentWorld = coords.getCanvas2world().transformPoint(pos);
+	params.center = params.center.add(dragStartWorld.subtract(currentWorld));
+	coords.recalc(params);
+	g.draw();
+    }
+    $(g.canvas).mousedown(function (e) {
+	var pos = getPosition(g.canvas, e);
+	dragStartWorld = coords.getCanvas2world().transformPoint(pos);
+	g.canvas.onmousemove = onMove;
+    }).mouseup(function (e) {
+	g.canvas.onmousemove = null;
+    }).mouseout(function (e) {
+	g.canvas.onmousemove = null;
+    }).bind('mousewheel', function (event, delta) {
+	var mouseCanvas = getPosition(g.canvas, event);
+	zoomAbout(g.canvas, params, coords, Math.pow(1.3, delta), mouseCanvas);
+	$("#scale").slider({value: params.scale});
+	g.draw();
+	return false;
+    });
 
-    $.each(["scale", "cx", "cy"], function(i,v) {
+    $("#scale").slider({min: .1, max: 30, step: .001});
+    $("#cx").slider({min: -122.5, max: -121.8, step: .001});
+    $("#cy").slider({min:37.2, max: 38.5, step: .001});
+
+    $.each(["scale"], function(i,v) {
 	$("#"+v).slider({value: params[v]});
 	$("#"+v).slider({slide: function (ev, ui) {
+
 	    params[v] = ui.value;
+
 	    $("#paramDisplay").text(JSON.stringify(params));
 
 	    coords.recalc(params);
@@ -236,14 +399,7 @@ $(function() {
 	}});
     });
 
-
-    var stomp = new STOMPClient();
-    stomp.onconnectedframe = function () {
-	stomp.subscribe('view');
-	$.post("updateTrails")
-    };
-    stomp.onmessageframe = function(frame) {
-	var data = JSON.parse(frame.body);
+    startStomp(function (data) {
 	$.extend(trailPoints, data.trailPoints);
 
 	coords.scale = data.scale;
@@ -252,8 +408,6 @@ $(function() {
 			    coords.recalc(params);
 			    g.draw();
 			});
-    };
-    stomp.connect('localhost', 61614);
-
+    }); 
 
 });
