f3d198bb8766e3305a02c83ef93a9ff60068be28
drewp <drewp@bigasterisk.com>
2014-06-21 14:55:50 -0700

    mqtt receiver logging
    Ignore-this: dc94eda35b9904d82ec1010682a627d7
    
    darcs-hash:20140621215550-312f9-4c2344f63133d4d1a436e8177eca16ef8964ffe4

--- mqtt_receiver.py
+++ mqtt_receiver.py
@@ -5,17 +5,22 @@ phone app sends to mosquitto broker
 we subscribe on that broker and send http to update.py
 """
 
-import mosquitto, json, restkit
+import mosquitto, json, restkit, logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+log = logging.getLogger()
 
 config = json.load(open('priv.json'))
-updateServer = restkit.Resource("http://localhost:9033/update")
+updateServer = restkit.Resource("http://bang:9033/update")
 
 client = mosquitto.Mosquitto("map-receiver")
-client.connect("bigasterisk.com")
+client.connect("prime.bigasterisk.com")
+log.info('connected to %s', client._host)
+# need more auth here, to be able to read
 client.subscribe("/mqttitude/#", 0)
 def on_message(mosq, obj, msg):
     payload = json.loads(msg.payload)
-    print "got message %r %r" % (msg.topic, payload)
+    log.info("got message %r %r", msg.topic, payload)
     userFromTopic = config['mqttTopic'][msg.topic]
     record = {
         "timestamp" : int(payload['tst']) * 1000,
@@ -27,9 +32,9 @@ def on_message(mosq, obj, msg):
     for attr in ['alt', 'batt', 'acc']:
         if attr in payload:
             record[attr] = payload[attr]
-    print record
+    log.info(repr(record))
     updateServer.post(payload=json.dumps(record))
-    print "finished message"
+    log.info("posted message")
 
 client.on_message = on_message
 
