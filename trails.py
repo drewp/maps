cbfe695f3113fc63fee4732c0616caf6886ca7ba
drewp <drewp@bigasterisk.com>
2017-08-22 12:56:40 -0700

    trails.py to cyclone
    Ignore-this: 4b9c82236e4f3ff00b2347c5d4ba470b
    
    darcs-hash:1f58767ae95a84ec812f7d78c248458b85ee8548

--- trails.py
+++ trails.py
@@ -4,12 +4,11 @@ from __future__ import division
 query for people's previous update points in mongodb
 """
 import json, re, time, logging, datetime, sys
-#import gevent.monkey
-#gevent.monkey.patch_all()
-from bottle import route, response, run, request
 from rdflib import Namespace, URIRef, Literal, RDFS
 from dateutil.tz import tzutc, tzlocal
 from pymongo import Connection
+import cyclone.web, cyclone.websocket
+from twisted.internet import reactor
 from describelocation import describeLocationFull, metersFromHome
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
@@ -33,55 +32,55 @@ if 0:
     def pt_sec(pt): return pt['recv_time']
     
 
-@route("/trails")
-def trails():
-    q = json.loads(request.query.q)
-    return getUpdateMsg(query=q)
+class Trails(cyclone.web.RequestHandler):
+    def get(self):
+        q = json.loads(self.get_argument('q'))
+        self.write(getUpdateMsg(query=q))
 
-@route("/graph")
-def graph():
-    log.debug("start graph")
-    g = StateGraph(URIRef("http://bigasterisk.com/map"))
+class GetGraph(cyclone.web.RequestHandler):
+    def get(self):
+        log.debug("start graph")
+        g = StateGraph(URIRef("http://bigasterisk.com/map"))
 
-    for user in [URIRef("http://bigasterisk.com/foaf.rdf#drewp"),
-                 URIRef("http://bigasterisk.com/kelsi/foaf.rdf#kelsi"),
-                 ]:
-        log.debug("find points for %s", user)
-        pt = list(mongo.find({'user':user}, sort=[TIME_SORT], limit=1))
-        if not pt:
-            continue
-        pt = pt[0]
-        t = datetime.datetime.fromtimestamp(
-            pt_sec(pt), tzutc()).astimezone(tzlocal())
-        g.add((user, MAP['lastSeen'], Literal(t)))
+        for user in [URIRef("http://bigasterisk.com/foaf.rdf#drewp"),
+                     URIRef("http://bigasterisk.com/kelsi/foaf.rdf#kelsi"),
+                     ]:
+            log.debug("find points for %s", user)
+            pt = list(mongo.find({'user':user}, sort=[TIME_SORT], limit=1))
+            if not pt:
+                continue
+            pt = pt[0]
+            t = datetime.datetime.fromtimestamp(
+                pt_sec(pt), tzutc()).astimezone(tzlocal())
+            g.add((user, MAP['lastSeen'], Literal(t)))
 
-        ago = int(time.time() - pt_sec(pt))
-        g.add((user, MAP['lastSeenAgoSec'], Literal(ago)))
-        g.add((user, MAP['lastSeenAgo'], Literal(
-            "%s seconds" % ago if ago < 60 else
-            ("%.1f minutes" % (ago / 60) if ago < 3600 else
-             ("%.1f hours" % (ago / 3600))))))
+            ago = int(time.time() - pt_sec(pt))
+            g.add((user, MAP['lastSeenAgoSec'], Literal(ago)))
+            g.add((user, MAP['lastSeenAgo'], Literal(
+                "%s seconds" % ago if ago < 60 else
+                ("%.1f minutes" % (ago / 60) if ago < 3600 else
+                 ("%.1f hours" % (ago / 3600))))))
 
-        log.debug("describeLocationFull")
-        desc, targetUri, targetName, dist = describeLocationFull(
-            config,
-            pt['longitude'], pt['latitude'],
-            pt.get('horizAccuracy', pt.get('accuracy', 0)),
-            str(user))
-        
-        g.add((user, MAP['lastNear'], targetUri))
-        g.add((targetUri, RDFS.label, Literal(targetName)))
-        g.add((user, MAP['lastDesc'], Literal(desc)))
-        g.add((user, MAP['distanceToHomeM'], Literal(metersFromHome(
-            config, user, pt['longitude'], pt['latitude']))))
-        if ago < 60*15:
-            g.add((user, MAP['recentlyNear'], targetUri))
-        log.debug("added %s", user)
-        
-    response.set_header("content-type", 'application/x-trig')
-    ret = g.asTrig()
-    log.debug("return graph")
-    return ret
+            log.debug("describeLocationFull")
+            desc, targetUri, targetName, dist = describeLocationFull(
+                config,
+                pt['longitude'], pt['latitude'],
+                pt.get('horizAccuracy', pt.get('accuracy', 0)),
+                str(user))
+
+            g.add((user, MAP['lastNear'], targetUri))
+            g.add((targetUri, RDFS.label, Literal(targetName)))
+            g.add((user, MAP['lastDesc'], Literal(desc)))
+            g.add((user, MAP['distanceToHomeM'], Literal(metersFromHome(
+                config, user, pt['longitude'], pt['latitude']))))
+            if ago < 60*15:
+                g.add((user, MAP['recentlyNear'], targetUri))
+            log.debug("added %s", user)
+
+        self.set_header("content-type", 'application/x-trig')
+        ret = g.asTrig()
+        log.debug("return graph")
+        self.write(ret)
 
 def getUpdateMsg(movingUser=None, query=None):
     trailPoints = {}
@@ -129,5 +128,11 @@ def filter_stale(recent):
             keep.append(r)
     return keep
 
-run(server='cherrypy',
-    host="0.0.0.0", port=9099)
+from twisted.python import log as twlog
+twlog.startLogging(sys.stdout)
+
+reactor.listenTCP(9099, cyclone.web.Application(handlers=[
+    (r'/trails', Trails),
+    (r'/graph', GetGraph),
+    ], debug=True), interface='::')
+reactor.run()
